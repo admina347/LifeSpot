e180fac0925df2a6d30a09d2e3f4e9d5a79305a1
admina <it-mactep@mail.ru>
2023-03-02 14:50:02 +0500

    test sync

--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -1,32 +1,79 @@
 /*
-* Функция для проверки и сохранения  данных пользователя
-* Также блокирует доступ к сайту лицам, не подтвердившим свой возраст
+* Проверка возраста пользователя
 *
 * */
+let checker = function ( newVisit ){
+    if(window.sessionStorage.getItem("userAge") >= 18 ){
+        // Добавим проверку на первое посещение, чтобы не показывать приветствие
+        // лишний раз
+        if(newVisit){
+            alert("Приветствуем на LifeSpot! " + '\n' +  "Текущее время: " + new Date().toLocaleString() );
+        }
+    }
+    else{
+        alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
+        window.location.href = "http://www.google.com"
+    }
+ }
 
 /*
 * Сессия теперь создается в общей области видимости.
 * Будет "захватываться" тремя функциями
 * 
 * */ 
-let session =  new Map();
+//let session =  new Map();
 
 /*
 * Сохранение данных сессии сразу при заходе пользователя на страницу
-* 
+*
 * */
-function handleSession(){
-    // Сохраним время начала сессии
-    session.set("startDate", new Date().toLocaleString())
-    // Сохраним UserAgent
-    session.set("userAgent", window.navigator.userAgent)
-}
+/* let session =  {
+    'startDate' : new Date().toLocaleString(),
+    'userAgent' : window.navigator.userAgent,
+    'userAge' : prompt("Пожалуйста, введите ваш возраст?")
+ }
+ */
+/*
+* Сохранение данных сессии сразу при заходе пользователя на страницу
+*
+* */
+function handleSession (logger, checker){
+  
+    // Проверяем дату захода и проставляем, если новый визит
+    if(window.sessionStorage.getItem("startDate") == null){
+        window.sessionStorage.setItem("startDate", new Date().toLocaleString())
+    }
+  
+    // Проверяем userAgent и проставляем, если новый визит
+    if(window.sessionStorage.getItem("userAgent") == null){
+        window.sessionStorage.setItem("userAgent", window.navigator.userAgent)
+    }
+  
+    // Проверяем возраст и проставляем, если новый визит
+    if(window.sessionStorage.getItem("userAge") == null){
+        let input = prompt("Пожалуйста, введите ваш возраст?");
+        window.sessionStorage.setItem("userAge", input)
+       
+        /* Возраст отсутствовал в sessionStorage. Значит, это первый визит пользователя, и
+         при прохождении проверки на возраст он увидит приветствие*/
+        checker(true)
+    }else{
+  
+        /* Пользователь заходит не первый раз, приветствие не показываем. */
+        checker(false)
+    }
+   
+    /* Вызываем переданную в качестве колл-бэка функцию логирования.
+        передавать в качестве коллбека не обязательно, можно вызвать и напрямую, но мы добавили для повторения.
+    */
+    logger()
+ }
 
 /*
 * Проверка возраста пользователя
 * 
 * */
-function checkAge(){
+/* function checkAge(){
     session.set("age", prompt("Пожалуйста, введите ваш возраст?"))
     
     if(session.get("age") >= 18){
@@ -36,16 +83,41 @@ function checkAge(){
         alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
         window.location.href = "http://www.google.com"
     }
-}
+} */
+
+/*
+* Проверка возраста пользователя
+*
+* */
+function checkAge(){
+    if(session.userAge >= 18){
+        alert("Приветствуем на LifeSpot! " + '\n' +  "Текущее время: " + new Date().toLocaleString() );
+    }
+    else{
+        alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
+        window.location.href = "http://www.google.com"
+    }
+ }
+
+/*
+* Вывод данных сессии в консоль
+*
+* */
+let logger = function () {
+    console.log('Начало сессии: ' + window.sessionStorage.getItem("startDate") )
+    console.log('Даныне клиента: ' + window.sessionStorage.getItem("userAgent") )
+    console.log('Возраст пользователя: ' + window.sessionStorage.getItem("userAge"))
+ }
+
 
 /* Вывод данных сессии в консоль
 * 
 * */
-let sessionLog = function logSession() {
+/* let sessionLog = function logSession() {
     for (let result of session){
         console.log(result)
     }
-}
+} */
 
 /*
 * Всплывающее окно будет показано по таймауту
